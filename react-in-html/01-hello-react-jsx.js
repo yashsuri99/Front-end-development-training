58d2d1e8147beff302413850a085d287025694cd
Yash Suri <yashsuri1999@gmail.com>
2022-03-07 11:38:43 +0530

    setting up react app

---
+++ react-in-html/01-hello-react-jsx.js
@@ -0,0 +1,29 @@
+// let tr = document.createElement('tr');
+// tr.setAttribute('id', 'message');
+
+// REACT element -> like a DOM node
+const hr = React.createElement("hr");
+
+// JSX -> JavascriptXML
+const div = (
+    <div id="message" className="alert">
+        Hello React
+        <hr />
+    </div>
+);
+
+// React elements are JS objects - they have type, props
+// Props includes the attribrutes, props.children has all the children in an array
+console.log(hr);
+console.log(div);
+
+hr.type = 'br';
+
+// React elements are immutable -> the value of their properties cannot be changed
+// try it - this gives an error
+// console.log(hr);  // no change in type
+
+// EXCERSISE: try to push a string to div.props.children -> not possible
+
+// 1st -> React element
+ReactDOM.render(div, document.getElementById("root"));
